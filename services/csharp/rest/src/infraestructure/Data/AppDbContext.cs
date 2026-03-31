23b5bf715a884ecef2f6a7e553d6c2513eef2e9a
Felipe Cardoso <felipetcardosoo@gmail.com>
2026-03-30 22:19:17 -0300

    adicionando leitura da tabela para modelo usado

--- services/csharp/rest/src/infraestructure/Data/AppDbContext.cs
+++ services/csharp/rest/src/infraestructure/Data/AppDbContext.cs
@@ -10,5 +10,34 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<Product> Products => Set<Product>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("products");
+            entity.HasKey(product => product.Id);
 
+            entity.Property(product => product.Id)
+                .ValueGeneratedOnAdd();
+
+            entity.Property(product => product.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            entity.Property(product => product.Description)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            entity.Property(product => product.Price)
+                .HasPrecision(18, 2)
+                .IsRequired();
+
+            entity.Property(product => product.StockQuantity)
+                .IsRequired();
+
+            entity.Property(product => product.CreatedAt)
+                .IsRequired();
+        });
+    }
 }
