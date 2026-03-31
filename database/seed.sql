c9b266598fd0a2dd82ec0cbb5872ad56e9f89190
Felipe Cardoso <felipetcardosoo@gmail.com>
2026-03-30 22:22:22 -0300

    adicionando comandos para alimentar o banco de dados

---
+++ database/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.products ("Name", "Description", "Price", "StockQuantity", "CreatedAt")
+VALUES
+    ('Notebook', 'Notebook para desenvolvimento', 4500.00, 10, NOW()),
+    ('Mouse', 'Mouse sem fio', 120.50, 25, NOW());
